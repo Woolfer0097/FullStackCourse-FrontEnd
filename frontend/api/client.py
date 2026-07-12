35da07aed94f3d47d0cf7a00377ffb3bc5542e07
Woolfer0097 <Woolfer0097@yandex.ru>
2026-07-12 10:38:47 +0300

    feat: api client

---
+++ frontend/api/client.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+BACKEND_URL = "http://127.0.0.1:8000"
+
+LOGIN_ENDPOINT = f"{BACKEND_URL}/auth/login/"
+REGISTER_ENDPOINT = f"{BACKEND_URL}/auth/register/"
+
+
+def register():
+    data = {
+      "email": "string1@email.com",
+      "password": "s1234535",
+      "full_name": "Danila"
+    }
+    response = requests.post(REGISTER_ENDPOINT, body=json.dumps(data)) # , headers={"Authorization": f"Bearer {token}"}
+    print(response.json())
+
+
+if __name__ == '__main__':
+    register()
